c388b00084cad0c3542d51fe42c655da0ddd5909
Leandro Brunner <leandrobrunner@yahoo.com.ar>
2015-02-13 00:15:36 -0300

    Update jquery.overlap.js

--- jquery.overlap.js
+++ jquery.overlap.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Overlap Plugin 0.1
+ * jQuery Overlap Plugin 0.2
  * https://github.com/leandrobrunner/jQuery-Overlap
  *
  * Copyright (c), Leandro Brunner
@@ -13,13 +13,15 @@
     $.fn.overlap = function(args) {
         args = $.extend({
             rect: {x:0, y:0, w:0, h:0},
+            element: false,
             mark: false,
             unmark: true,
             filterMark: function(index) {return true},
             filterUnmark: function(index) {return true},
             callback: function($elements) {},
-            w: function(element) {return element.outerWidth(true)},
-            h: function(element) {return element.outerHeight(true)},
+            w: function($element) {return $element.outerWidth(true)},
+            h: function($element) {return $element.outerHeight(true)},
+            position: function($element) {return $element.position()},
             bring: true 
         }, args);
         Object.prototype.overlap = function(rect) {
@@ -28,8 +30,21 @@
                 (rect.y <= this.y + this.h && rect.y + rect.h >= this.y)
             );
         };
+        if(args.element!=false) {
+            if(typeof(args.element)=="string") {
+                args.element=$(args.element);
+            }
+            args.element=args.element.first();
+            var pos = args.position(args.element);
+            args.rect={
+                x: pos.left,
+                y: pos.top,
+                w: args.w(args.element),
+                h: args.h(args.element)
+            };
+        }
         var $selects = this.filter(function(index) {
-            var pos = $(this).position();
+            var pos = args.position($(this));
             if(
                 args.rect.overlap({
                     x: pos.left,
@@ -54,4 +69,4 @@
             return(this);
         }
     };
-}(jQuery)); 
+}(jQuery));
